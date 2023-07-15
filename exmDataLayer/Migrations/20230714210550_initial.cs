eeded833a9c86dfdbf7edc98a3e678d223939a32
mhOsama62 <139636240+mhOsama62@users.noreply.github.com>
2023-07-15 23:51:04 +0300

    Add files via upload

---
+++ exmDataLayer/Migrations/20230714210550_initial.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exmDataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Gear = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Km = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Custmor",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Custmor", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Total = table.Column<double>(type: "float", nullable: false),
+                    CarID = table.Column<int>(type: "int", nullable: false),
+                    CustmorID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Sales_Cars_CarID",
+                        column: x => x.CarID,
+                        principalTable: "Cars",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sales_Custmor_CustmorID",
+                        column: x => x.CustmorID,
+                        principalTable: "Custmor",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Quntity = table.Column<int>(type: "int", nullable: false),
+                    SuppliersID = table.Column<int>(type: "int", nullable: false),
+                    Carid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parts", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Parts_Cars_Carid",
+                        column: x => x.Carid,
+                        principalTable: "Cars",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Parts_Suppliers_SuppliersID",
+                        column: x => x.SuppliersID,
+                        principalTable: "Suppliers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parts_Carid",
+                table: "Parts",
+                column: "Carid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parts_SuppliersID",
+                table: "Parts",
+                column: "SuppliersID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_CarID",
+                table: "Sales",
+                column: "CarID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_CustmorID",
+                table: "Sales",
+                column: "CustmorID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parts");
+
+            migrationBuilder.DropTable(
+                name: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Custmor");
+        }
+    }
+}
---
+++ exmDataLayer/class1.cs
@@ -0,0 +1,22 @@
+﻿using examDomin;
+using Microsoft.EntityFrameworkCore;
+
+namespace exmDataLayer
+{
+    public class examDBContext : DbContext 
+    {
+        public DbSet<Cars> Cars { get; set; }
+        public DbSet<Custmor> Custmor { get; set; }
+        public DbSet<Parts> Parts { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<Suppliers> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (LocalDB)\\MSSQLLocalDB; Initial Catalog = examDB");
+        }
+
+
+
+    }
+}
