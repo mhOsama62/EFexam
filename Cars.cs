4b36222137d5d4de94a1c8c72b081cf568b1d441
mhOsama62 <139636240+mhOsama62@users.noreply.github.com>
2023-07-15 23:42:12 +0300

    Add files via upload

---
+++ Cars.cs
@@ -0,0 +1,14 @@
+﻿namespace examDomin
+{
+    public class Cars
+    {
+        public int id { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Gear { get; set; }
+        public string Km { get; set; }
+
+        // ربط جدول Cars بجدول Parts
+        public virtual List<Parts> Parts { get; set; }
+    }
+}
