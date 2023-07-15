b16f4a001a1780de1f2d9fc489f1d54f496d125f
mhOsama62 <139636240+mhOsama62@users.noreply.github.com>
2023-07-15 23:57:09 +0300

    Add files via upload

---
+++ examDomin/Parts.cs
@@ -0,0 +1,18 @@
+﻿namespace examDomin
+{
+    public class Parts
+    {
+        public int Carid { get; set; }
+        public int id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int SuppliersID { get; set; }
+
+        // ربط جدول Parts بجدول Cars
+        public virtual Cars Car { get; set; }
+
+        // ربط جدول Parts بجدول Suppliers
+        public virtual Suppliers Supplier { get; set; }
+    }
+}
---
+++ examDomin/Suppliers.cs
@@ -0,0 +1,12 @@
+﻿namespace examDomin
+{
+    public class Suppliers
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        // ربط جدول Suppliers بجدول Parts
+        public virtual List<Parts> Parts { get; set; }
+    }
+}
